5efc25a8e16195b62fc545394c4483553df774a3
Zeha Irawan <jangkaringbumi@gmail.com>
2020-07-22 08:52:10 +0700

    initial test

---
+++ functions/server.js
@@ -0,0 +1,90 @@
+const serverless = require('serverless-http');
+const bodyParser = require('body-parser');
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const { check, validationResult } = require('express-validator');
+const config = require('config');
+const { connectDB, closeDB } = require('../config/db');
+
+const app = express();
+const router = express.Router();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// API calls
+router.get('/api/hello', (req, res) => {
+  res.send({ express: 'Hello From Express' });
+});
+
+router.post('/api/world', (req, res) => {
+  res.send(
+    `I received your POST request. This is what you sent me: ${req.body.post}`,
+  );
+});
+
+// @route    POST api/auth
+// @desc     Authenticate user & get token
+// @access   Public
+router.post(
+  '/api/auth',
+  [
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password is required').exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      connectDB();
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
+      }
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: '5 days' },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        },
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    } finally {
+      closeDB();
+    }
+  },
+);
+
+app.use('/.netlify/functions/server', router); // path must route to lambda
+
+module.exports = app;
+module.exports.handler = serverless(app);
